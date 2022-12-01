71d6631659879f845cde9bd6876fcee43d5e7aca
Yohann HUEDRO <112839213+yohann-devweb3@users.noreply.github.com>
2022-12-02 00:09:54 +1100

    update

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let urlApi = 'https://6057e432c3f49200173ad08d.mockapi.io/api/v1/employees/';
 let hiddenValue;
 let fname;
 let lname;
-let ejob;
-let mail;
+let userJob;
+let userMail;
 
 
 /*when page is ready and than all contents are loaded*/
@@ -12,60 +12,20 @@ $(document).ready(function () {
 
     getData();
 
+    
 
     $(".btnProfileUser").click(function (event) {
      
     event.preventDefault();
+    info("notification is-info has-text-centered", "Profile", "", "Unlock (for updating)");
 
-        myForm = '';
-        myForm = `
-        <form class="field">
-
-        <fieldset disabled id="fieldSetProfile">
-
-            <div class="field">
-            <label class="label">Firstname</label>
-                <div class="control">
-                <input id="sai_firstname" class="input" type="text" value="">
-                </div>
-            </div>
-
-            <div class="field">
-            <label class="label">Lastname</label>
-                <div class="control">
-                <input id="sai_lastname" class="input" type="text">
-                </div>
-            </div>
-
-            <div class="field">
-            <label class="label">Job</label>
-                <div class="control">
-                <input class="input" id="sai_job" type="text">
-                </div>
-            </div>
-
-            <div class="field">
-            <label class="label">Email</label>
-                <div class="control">
-                <input class="input" id="sai_email" type="text">
-                </div>
-            </div>
-        </fieldset>
-        </form>
-        `;
-
-
-        info("notification is-info has-text-centered", "Profile", "", "Update");
-  
-        $("#modal-card-body").append(myForm);
-        getDataEmp(hiddenValue);
-
+       generateForm(true);
     });
 
 
 
     //modal : waiting for deleting an employee or not
-    $(".btnDeleteUser").click(function (event) {
+    $(".btnDeleteEmp").click(function (event) {
         event.preventDefault();
         info("notification is-danger", "Warning", "Are you sure you want to delete this user?", "Yes (delete)");
     });
@@ -82,11 +42,34 @@ $(document).ready(function () {
         let btnTextConfirmModal = event.target.innerText;
 
         if (btnTextConfirmModal == "Yes (delete)") {
-            deleteUser(hiddenValue);
+            deleteEmp(hiddenValue);
+            $(".modal").hide();
         }
 
-        if (btnTextConfirmModal == "Update") {
-            $('.fieldSetProfile').setAttribute('disabled', '');
+        if (btnTextConfirmModal == "Unlock (for updating)") {
+             $('#fieldSetProfile').prop("disabled", false);
+             $('#btnConfirmModal').text("Save changes");
+      
+        }
+
+        if (btnTextConfirmModal == "Save changes") {
+            event.preventDefault();
+      
+        
+           info("notification is-danger", "Warning", "Are you sure you want to update this user?", "Yes (update)")
+           $('#fieldSetProfile').prop("disabled", false);
+           generateForm(false);
+         $('#sai_firstname').val(fname);
+            $('#sai_lastname').val(lname);
+            $('#sai_job').val(userJob);
+            $('#sai_email').val(userMail);
+         
+        }
+
+        if (btnTextConfirmModal == "Yes (update)") {
+            $('#fieldSetProfile').prop("disabled", false);
+   
+             updateEmp(hiddenValue);
         }
     });
 
@@ -96,16 +79,68 @@ $(document).ready(function () {
     });
 
 
-    $('#btnConfirmModal').click(function (event) {
-        $(".modal").hide();
-    });
+
 
 });
 
 
+//refresh the list of employees
+$("#btnRefreshAllEmp").click(function (event) {
+   
+    getData();
+});
 
 /*----------functions----------*/
-function getDataEmp(idEmployee){
+function generateForm(paramBool){
+   let myForm = '';
+    myForm = `
+    <form class="field">
+
+    <fieldset disabled id="fieldSetProfile">
+
+        <div class="field">
+        <label class="label">Firstname</label>
+            <div class="control">
+            <input id="sai_firstname" class="input" type="text" value="">
+            </div>
+        </div>
+
+        <div class="field">
+        <label class="label">Lastname</label>
+            <div class="control">
+            <input id="sai_lastname" class="input" type="text">
+            </div>
+        </div>
+
+        <div class="field">
+        <label class="label">Job</label>
+            <div class="control">
+            <input class="input" id="sai_job" type="text">
+            </div>
+        </div>
+
+        <div class="field">
+        <label class="label">Email</label>
+            <div class="control">
+            <input class="input" id="sai_email" type="text">
+            </div>
+        </div>
+    </fieldset>
+    </form>
+    `;
+   
+        $("#modal-card-body").append(myForm);
+        getDataEmp(hiddenValue,paramBool);
+    
+   
+   
+       
+    
+   
+
+}
+
+function getDataEmp(idEmployee,boolOk){
     
     myUrl = urlApi + idEmployee.toString();
 
@@ -116,10 +151,24 @@ function getDataEmp(idEmployee){
         async: false,
 
         success: function (response) {
-            $('#sai_firstname').val(response.name);
-            $('#sai_lastname').val(response.last_name);
-            $('#sai_job').val(response.job_title);
-            $('#sai_email').val(response.email);
+            if (boolOk===true){
+                $('#sai_firstname').val(response.name);
+                $('#sai_lastname').val(response.last_name);
+                $('#sai_job').val(response.job_title);
+                $('#sai_email').val(response.email);
+          
+        
+            }else{
+                $('#sai_firstname').val(this.val());
+                $('#sai_lastname').val(this.val());
+                $('#sai_job').val(this.val());
+                $('#sai_email').val(this.val());
+            }
+     
+
+
+
+          
         }
 
 
@@ -136,9 +185,9 @@ function info(classAlert, textTitreModal, textMessage, textButtonConfirm) {
     $(".modal").show();
 }
 
-function deleteUser(myId) {
+function deleteEmp(myId) {
     myUrl = urlApi + myId.toString();
-    alert(myUrl);
+
     $.ajax({
         type: "DELETE",
         url: myUrl,
@@ -151,7 +200,39 @@ function deleteUser(myId) {
             console.log("ERROR:" + error);
         }
     });
-    window.location.reload();
+  
+}
+
+function updateEmp(myId){
+    myUrl = urlApi + myId.toString();
+
+
+
+
+    $.ajax({
+        type: "PUT",
+        url: myUrl,
+        data: {
+            "id": myId,
+            "name": $('#sai_firstname').val(),
+            "last_name": $('#sai_lastname').val(),
+            "job_title": $('#sai_job').val(),
+            "email": $('#sai_email').val()
+            
+
+    
+        },
+        async: false,
+        success: function ($mockData) {
+              $('.modal').hide();
+            getData();
+          
+        },
+        error: function (request, error) {
+            console.log("ERROR:" + error);
+        }
+    });
+   
 }
 
 /*------------------------------------
@@ -181,14 +262,15 @@ function getData() {
         async: false,
 
         success: function (response) {
+            $('#tableData').empty();// empty table before load it
             $.each(response, function (key, value) {
-
+               
                 $('#tableData').append("<tbody><tr>\
                             <td>"+ value.id + "</td>\
                             <td>"+ value.name + "</td>\
                             <td>"+ value.last_name + "</td>" +
                     `<td><a href="" onclick='setHiddenId(` + value.id + `)' class="btnProfileUser mr-5"><i class="fa-solid fa-circle-info"></a></i>` +
-                    `<a href="" onclick='setHiddenId(` + value.id + `)' class="btnDeleteUser"><i class="fa-solid fa-user-xmark"></a></i>` + "</td>" +
+                    `<a href="" onclick='setHiddenId(` + value.id + `)' class="btnDeleteEmp"><i class="fa-solid fa-user-xmark"></a></i>` + "</td>" +
                     "</tr></tbody");
 
             })
