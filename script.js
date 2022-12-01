756d308de42d3ca42f2c77513c9169fb0a3c5bdd
Yohann HUEDRO <112839213+yohann-devweb3@users.noreply.github.com>
2022-12-01 19:07:15 +1100

    UPDATE : add view employee

--- script.js
+++ script.js
@@ -1,45 +1,102 @@
 /*----------variables------*/
 let urlApi = 'https://6057e432c3f49200173ad08d.mockapi.io/api/v1/employees/';
-let  hiddenValue;
+let hiddenValue;
+let fname;
+let lname;
+let ejob;
+let mail;
 
 
 /*when page is ready and than all contents are loaded*/
 $(document).ready(function () {
 
     getData();
+
+
+    $(".btnProfileUser").click(function (event) {
+     
+    event.preventDefault();
+
+        myForm = '';
+        myForm = `
+        <form class="field">
+
+        <fieldset disabled id="fieldSetProfile">
+
+            <div class="field">
+            <label class="label">Firstname</label>
+                <div class="control">
+                <input id="sai_firstname" class="input" type="text" value="">
+                </div>
+            </div>
+
+            <div class="field">
+            <label class="label">Lastname</label>
+                <div class="control">
+                <input id="sai_lastname" class="input" type="text">
+                </div>
+            </div>
+
+            <div class="field">
+            <label class="label">Job</label>
+                <div class="control">
+                <input class="input" id="sai_job" type="text">
+                </div>
+            </div>
+
+            <div class="field">
+            <label class="label">Email</label>
+                <div class="control">
+                <input class="input" id="sai_email" type="text">
+                </div>
+            </div>
+        </fieldset>
+        </form>
+        `;
+
+
+        info("notification is-info has-text-centered", "Profile", "", "Update");
   
+        $("#modal-card-body").append(myForm);
+        getDataEmp(hiddenValue);
 
-    
-    $(".btnDeleteUser").click(function(event) {
-        event.preventDefault();
-        info("notification is-danger","Warning","Are you sure you want to delete this user?","Yes (delete)");
     });
 
 
-    $("#btnCancelModal").click(function(event) {
-        event.preventDefault();
-        $("#image-modal").hide(); 
-    });
-    
-    $("#btnConfirmModal").click(function(event) {
-        event.preventDefault();
-        let btnTextConfirmModal=event.target.innerText;
-      
 
-        if(btnTextConfirmModal=="Yes (delete)"){
-          deleteUser(hiddenValue);
+    //modal : waiting for deleting an employee or not
+    $(".btnDeleteUser").click(function (event) {
+        event.preventDefault();
+        info("notification is-danger", "Warning", "Are you sure you want to delete this user?", "Yes (delete)");
+    });
+
+    //close the modal
+    $("#btnCancelModal").click(function (event) {
+        event.preventDefault();
+        $("#image-modal").hide();
+    });
+
+    //according the text of btnConfirmModal , execute a choice...
+    $("#btnConfirmModal").click(function (event) {
+        event.preventDefault();
+        let btnTextConfirmModal = event.target.innerText;
+
+        if (btnTextConfirmModal == "Yes (delete)") {
+            deleteUser(hiddenValue);
         }
 
- 
+        if (btnTextConfirmModal == "Update") {
+            $('.fieldSetProfile').setAttribute('disabled', '');
+        }
     });
-    
+
     //cancel the modal
-    $("#btnCloseModal").click(function(event) {
+    $("#btnCloseModal").click(function (event) {
         $(".modal").hide();
     });
-    
-    
-    $('#btnConfirmModal').click(function(event) {
+
+
+    $('#btnConfirmModal').click(function (event) {
         $(".modal").hide();
     });
 
@@ -48,31 +105,50 @@ $(document).ready(function () {
 
 
 /*----------functions----------*/
+function getDataEmp(idEmployee){
+    
+    myUrl = urlApi + idEmployee.toString();
+
+    $.ajax({
+        url: myUrl,
+        type: "GET",
+        cache: false,
+        async: false,
+
+        success: function (response) {
+            $('#sai_firstname').val(response.name);
+            $('#sai_lastname').val(response.last_name);
+            $('#sai_job').val(response.job_title);
+            $('#sai_email').val(response.email);
+        }
 
 
-function info(classAlert,textTitreModal,textMessage,textButtonConfirm) {
-$("#modal-card-head").removeClass();
-$("#modal-card-head").addClass("modal-card-head "+classAlert);
-$("#modal-card-title").text(textTitreModal);
-$("#modal-card-body").text(textMessage);
-$('#btnConfirmModal').text(textButtonConfirm);
-
-    $(".modal").show(); 
+    });
 }
 
-function  deleteUser(myId){
-    myUrl=urlApi+myId.toString();
+function info(classAlert, textTitreModal, textMessage, textButtonConfirm) {
+    $("#modal-card-head").removeClass();
+    $("#modal-card-head").addClass("modal-card-head " + classAlert);
+    $("#modal-card-title").text(textTitreModal);
+    $("#modal-card-body").text(textMessage);
+    $('#btnConfirmModal').text(textButtonConfirm);
+
+    $(".modal").show();
+}
+
+function deleteUser(myId) {
+    myUrl = urlApi + myId.toString();
     alert(myUrl);
     $.ajax({
-        type:   "DELETE",
-        url:    myUrl,
-        data:   {"id": myId},
+        type: "DELETE",
+        url: myUrl,
+        data: { "id": myId },
         async: false,
         success: function ($mockData) {
-           getData();
+            getData();
         },
         error: function (request, error) {
-            console.log ("ERROR:" + error);
+            console.log("ERROR:" + error);
         }
     });
     window.location.reload();
@@ -84,9 +160,9 @@ description : set the hidden id value to manipulate id from users
 parameters : none
 returns :  none
 --------------------------------------*/
-function setHiddenId(myId){
-    hiddenValue='';
-    hiddenValue=myId;
+function setHiddenId(myId) {
+    hiddenValue = '';
+    hiddenValue = myId;
 }
 
 
@@ -104,23 +180,22 @@ function getData() {
         cache: false,
         async: false,
 
-        
         success: function (response) {
             $.each(response, function (key, value) {
-            
+
                 $('#tableData').append("<tbody><tr>\
                             <td>"+ value.id + "</td>\
                             <td>"+ value.name + "</td>\
-                            <td>"+ value.last_name + "</td>"+
-                            `<td><a href="" onclick='setHiddenId(`+value.id+`)' class="btnDeleteUser mr-5"><i class="fa-solid fa-user-xmark"></a></i>`+
-                            `<a href="" onclick='setHiddenId(`+value.id+`)' class="btnDeleteUser"><i class="fa-solid fa-user-xmark"></a></i>`+"</td>"+
-                            "</tr></tbody");
-                            
+                            <td>"+ value.last_name + "</td>" +
+                    `<td><a href="" onclick='setHiddenId(` + value.id + `)' class="btnProfileUser mr-5"><i class="fa-solid fa-circle-info"></a></i>` +
+                    `<a href="" onclick='setHiddenId(` + value.id + `)' class="btnDeleteUser"><i class="fa-solid fa-user-xmark"></a></i>` + "</td>" +
+                    "</tr></tbody");
+
             })
-         
-         
+
+
         }
-   
+
 
     });
 
