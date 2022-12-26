6458419fc80cd31821f8127da8f3f6afd4043a08
unknown <yohann.huedro@gmail.com>
2022-12-26 20:26:46 +1100

    feat(ui): ReadMe added & cleanUp (W3C verified)

---
+++ README.md
@@ -0,0 +1,41 @@
+
+# API Employees project
+
+### Description : 
+This app allows to create a dynamic listing of a company's employees.This app creates a dynamic page using HTML and JavaScript. It uses AJAX and the DOM for the consultation, creation, modification and deletion of data.This web page is available on desktop and mobile.
+
+### Installation : 
+
+#### From git command 
+- Copy & paste to your terminal : ```git clone "https://github.com/yohann-devweb3/api-employees"```
+
+- All you need is already into the folder "api-employees" : 
+    - index.html
+    - script.js
+    - style.css
+Just execute the file named 'index.html' with your web brower.
+### Important !
+
+Make sure you have these scripts into the index.html file at then end just before the closing tag '</body>'
+
+```
+  <!--..........scripts js here......-->
+  <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="
+    crossorigin="anonymous"></script>
+  <script src="./script.js"></script>
+```
+
+### API References : 
+
+
+- `GET /api/v1/employees`
+- `GET /api/v1/employees/:id`
+- `POST /api/v1/employees`
+- `PUT /api/v1/employees/:id`
+- `DELETE /api/v1/employees/:id`
+
+Enjoy the app~
+## Authors
+
+- [@Yohann_HUEDRO](https://github.com/yohann-devweb3)
+
--- script.js
+++ script.js
@@ -2,37 +2,27 @@
 let urlApi = 'https://6057e432c3f49200173ad08d.mockapi.io/api/v1/employees/';
 let hiddenValue;
 
-
-
 /*when page is ready and than all contents are loaded*/
 $(document).ready(function (event) {
-
     getData();
-
     //display the form of employee's Profile 
     $(".btnProfileUser").click(function (event) {
-  
-     
-        hiddenValue=$(event.currentTarget).data('id');
+        hiddenValue = $(event.currentTarget).data('id');
         event.preventDefault();
-      
-        info("notification is-info has-text-centered", "Profile", "", "Unlock (for updating)");  
+        info("notification is-info has-text-centered", "Profile", "", "Unlock (for updating)");
         generateForm(true);
-
     });
 
     //display the form's Profile 
     $("#btnAskAddEmp").click(function (event) {
-
         event.preventDefault();
         info("notification is-info has-text-centered", "Add an employee", "", "Add");
         generateForm(false);
     });
 
-
     //modal : waiting for deleting an employee or not
     $(".btnDeleteEmp").click(function (event) {
-        hiddenValue=$(event.currentTarget).data('id');
+        hiddenValue = $(event.currentTarget).data('id');
         event.preventDefault();
         info("notification is-danger", "Warning", "Are you sure you want to delete this user?", "Yes (delete)");
     });
@@ -41,10 +31,7 @@ $(document).ready(function (event) {
     $("#btnCancelModal").click(function (event) {
         event.preventDefault();
         $("#image-modal").hide();
-
     });
-
-
 
     //according the text of btnConfirmModal , execute a choice...
     $("#btnConfirmModal").click(function (event) {
@@ -52,16 +39,14 @@ $(document).ready(function (event) {
         let btnTextConfirmModal = event.target.innerText;
 
         if (btnTextConfirmModal == "Yes (delete)") {
-       
-            deleteEmp(hiddenValue);
+            deleteEmp(hiddenValue);//delete an user
             $(".modal").hide();
-            message("Employee deleted ! ","notification  is-success  is-verticaled ");
+            message("Employee deleted ! ", "notification  is-success  is-verticaled ");
         }
 
-        if (btnTextConfirmModal == "Unlock (for updating)") {
+        if (btnTextConfirmModal == "Unlock (for updating)") {//unlock form user
             $('#fieldSetProfile').prop("disabled", false);
             $('#btnConfirmModal').text("Save changes");
-
         }
 
         if (btnTextConfirmModal == "Save changes") {
@@ -76,8 +61,8 @@ $(document).ready(function (event) {
         //update the employee according params we give it into the form
         if (btnTextConfirmModal == "Yes (update)") {
             $('#fieldSetProfile').prop("disabled", false);
-            updateEmp(hiddenValue, fname, lname, empMail, empJob);
-            message("Informations updated ! ","notification  is-success  is-verticaled");
+            updateEmp(hiddenValue, fname, lname, empMail, empJob);//update user
+            message("Informations updated ! ", "notification  is-success  is-verticaled");
         }
 
         //ask by a modal the need to add an employee
@@ -93,7 +78,7 @@ $(document).ready(function (event) {
         if (btnTextConfirmModal == "Yes (add)") {
             $('#fieldSetProfile').prop("disabled", false);
             addEmp(fname, lname, empMail, empJob);
-            message("Employee added ! ","notification  is-success  is-verticaled");
+            message("Employee added ! ", "notification  is-success  is-verticaled");
         }
     });
 
@@ -102,53 +87,42 @@ $(document).ready(function (event) {
         $(".modal").hide();
     });
 
-
     //close the modal by little button close on top side
     $("#btnCloseTopModal").click(function (event) {
         $(".modal").hide();
     });
-
 });
 
 
 //refresh the list of employees
 $("#btnRefreshAllEmp").click(function (event) {
-     event.preventDefault();
-   
+    event.preventDefault();
     getData();
-    message("Refreshing... ","notification  is-success ");
-
+    message("Refreshing... ", "notification  is-success ");
 });
 
 /*----------functions----------*/
-function message(message, classMessage) {
 
+//display a msg then hide after 1.35seconds
+function message(message, classMessage) {
     $('#messageAlert').removeClass();
     $('#messageAlert').addClass(classMessage + ' columns is-vcentered ');
-    $('#messageAlert').css('visibility','visible');
+    $('#messageAlert').css('visibility', 'visible');
     $('#messageAlert').text(message);
     setTimeout(() => {
-
-      
-        $('#messageAlert').css('visibility','hidden');
-    // window.location.reload();
+        $('#messageAlert').css('visibility', 'hidden');
     }, 1350);
- 
 }
 
-
+//generate a form : set the parameter to true to disable all inputs from the form and set false for  the reverse situation
 function generateForm(paramBool) {
-
     let myForm = '';
-
     if (paramBool === true) {
         myForm = myForm + `<div class="field"><fieldset disabled id="fieldSetProfile">`;
     } else {
         myForm = myForm + `<div class="field"><fieldset  id="fieldSetProfile">`;
-       hiddenValue="";
+        hiddenValue = "";
     }
-
-
 
     myForm = myForm +
         `   <div class="field ">
@@ -182,19 +156,13 @@ function generateForm(paramBool) {
     </div>
     `;
 
-    $("#modal-card-body").append(myForm);
-
-
+    $("#modal-card-body").append(myForm);//inject html strings from above to the form "myForm"
     getDataEmp(hiddenValue, paramBool);
-
-
-
 }
 
+//get the data from one employee with api /get : the parameter to access is 'idEmployee'
 function getDataEmp(idEmployee, boolOk) {
-
     myUrl = urlApi + idEmployee.toString();
-
     $.ajax({
         url: myUrl,
         type: "GET",
@@ -207,47 +175,42 @@ function getDataEmp(idEmployee, boolOk) {
                 $('#sai_lastname').val(response.last_name);
                 $('#sai_job').val(response.job_title);
                 $('#sai_email').val(response.email);
-
             }
-
         }
-
-
     });
 }
 
+//configure & display a message (design & description & title)
 function info(classAlert, textTitreModal, textMessage, textButtonConfirm) {
     $("#modal-card-head").removeClass();
     $("#modal-card-head").addClass("modal-card-head " + classAlert);
     $("#modal-card-title").text(textTitreModal);
     $("#modal-card-body").text(textMessage);
     $('#btnConfirmModal').text(textButtonConfirm);
-
     $('.modal').addClass('modal columns is-vcentered');
     $(".modal").show();
 }
 
+//delete a employee by api :/delete
 function deleteEmp(myId) {
     myUrl = urlApi + myId.toString();
-
     $.ajax({
         type: "DELETE",
         url: myUrl,
         data: { "id": myId },
         async: false,
         success: function ($mockData) {
-            getData();
+            getData();//refresh grid employees
         },
         error: function (request, error) {
             console.log("ERROR:" + error);
         }
     });
-
 }
 
+//update the employee's informations by api :'/put'
 function updateEmp(myId, fname, lname, emailEmp, jobEmp) {
     myUrl = urlApi + myId.toString();
-
     $.ajax({
         type: "PUT",
         url: myUrl,
@@ -259,68 +222,49 @@ function updateEmp(myId, fname, lname, emailEmp, jobEmp) {
             "last_name": lname,
             "job_title": jobEmp,
             "email": emailEmp
-
         },
         async: false,
         success: function ($mockData) {
-            $('.modal').hide();
-          //  window.location.reload();
-            getData();
+            $('.modal').hide();//hide modal
+            getData();//refresh grid employees
 
         },
         error: function (request, error) {
             console.log("ERROR:" + error);
         }
     });
-
 }
 
+// add an employee with api /post
 function addEmp(fname, lname, emailEmp, jobEmp) {
     myUrl = urlApi;
-
     $.ajax({
         type: "POST",
         url: myUrl,
         data: {
-
             "name": fname,
             "last_name": lname,
             "job_title": jobEmp,
             "email": emailEmp
-
         },
         async: false,
         success: function ($mockData) {
-            $('.modal').hide();
-           // window.location.reload();
-            getData();
-          
+            $('.modal').hide(); //hide modal
+            getData();//refresh grid employees
         },
         error: function (request, error) {
             console.log("ERROR:" + error);
         }
     });
-
 }
 
-
-
-
-/*------------------------------------
-function name : getData()
-description : data object into the table "tableData"
-parameters : none
-returns :  data object into the table "tableData"
---------------------------------------*/
-
+//get the data from all employees with api /get
 function getData() {
- 
     $.ajax({
         url: urlApi,
         type: "GET",
         cache: false,
         async: false,
-
         success: function (response) {
             $('#tableData').empty();// empty table before load it
             let tHead =
@@ -335,21 +279,14 @@ function getData() {
 
             $('#tableData').append(tHead);
             $.each(response, function (key, value) {
-
                 $('#tableData').append("<tbody><tr>\
                             <td>"+ value.id + "</td>\
                             <td>"+ value.name + "</td>\
                             <td>"+ value.last_name + "</td>" +
-                    `<td><a href=''  data-id='`+ value.id +`'  class="btnProfileUser mr-5"><i class="fa-solid fa-circle-info"></i></a>` +
-                    `<a href=''  data-id='`+ value.id +`'   class="btnDeleteEmp"><i class="fa-solid fa-user-xmark"></i></a>` + "</td>" +
+                    `<td><a href=''  data-id='` + value.id + `'  class="btnProfileUser mr-5"><i class="fa-solid fa-circle-info"></i></a>` +
+                    `<a href=''  data-id='` + value.id + `'   class="btnDeleteEmp"><i class="fa-solid fa-user-xmark"></i></a>` + "</td>" +
                     "</tr></tbody")
-
             })
-
-
         }
-
-
     });
-  
 }
